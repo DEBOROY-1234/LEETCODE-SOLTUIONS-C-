05537fe29971dcfda575b7bf1091edf34d4d4771
DEBOROY-1234 <deboroy2002@gmail.com>
2023-03-20 19:29:44 +0530

    94. Binary Tree Inorder Traversal

---
+++ leetcode-31.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*>s;
+        TreeNode* node=root;
+        while(true){
+            if(node!=NULL){
+                s.push(node);
+                node=node->left;
+            }
+            else{
+                if(s.empty()) break;
+                node=s.top(); 
+                s.pop();
+                ans.push_back(node->val);
+                node=node->right;
+            }
+        }
+            return ans;
+
+    }
+};
+
