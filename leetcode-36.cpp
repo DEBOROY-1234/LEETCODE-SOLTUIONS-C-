0d7ccec2100efb42da089674b1489cad65f89203
DEBOROY-1234 <deboroy2002@gmail.com>
2023-03-25 22:58:49 +0530

    1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree

---
+++ leetcode-36.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    TreeNode* required;
+
+    void findNode(TreeNode* cloned , TreeNode* target){
+        if(cloned==NULL){
+            return;
+        }
+        if(cloned->val==target->val){
+            required=cloned;
+        }
+        findNode(cloned->left , target);
+        findNode(cloned->right , target);
+    }
+
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+
+        findNode(cloned , target);
+        return required;
+
+    }
+};
